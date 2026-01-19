1ef8dfc776b401136ef48362a2faf77bd07f7a8a
Liu-Fernando <fernandoliu@usp.br>
2026-01-19 17:04:06 -0300

    Linkando página de template universal e retirandi padding do header

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def home():
 def paginaInicial():
     return render_template("paginaInicial.html")
 
+#Páginas saúde Mental
+
 @app.route("/saudeCerebral")
 def saudeCerebral():
         return render_template("saudeMental_Cerebral.html")
@@ -36,6 +38,11 @@ def Linha_Do_Tempo():
 @app.route("/minhaGaleria")
 def Minha_Galeria():
         return render_template("minhaGaleria.html")
+
+#Teste de template Universal no botão de Jogos cognitivos
+@app.route("/jogosCognitivos")
+def jogosCognitivos():
+        return render_template("saudeMental_templateUniversal.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
