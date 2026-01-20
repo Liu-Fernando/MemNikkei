4fe88f393c2a03101f00b728f55e810a49767ed4
Liu-Fernando <fernandoliu@usp.br>
2026-01-19 21:41:14 -0300

    Adicionando link para páginas, formatando nomes de arquivos, trocando href para url_for

--- app.py
+++ app.py
@@ -11,25 +11,20 @@ def home():
 def paginaInicial():
     return render_template("paginaInicial.html")
 
-#Relembrar Tradições
+##################################  RELEMBRAR TRADIÇÕES  ##############################
+
+@app.route("/relembrarTradicoes")
+def relembrarTradicoes():
+        return render_template("relembrarTradicoes.html")
 @app.route("/linhaDoTempo")
 def linhaDoTempo():
         return render_template("linhaDoTempo.html")
 
-#Páginas saúde Mental
-@app.route("/saudeMental")
-def saudeMental():
-        return render_template("saudeMental.html")
-
-@app.route("/saudeCerebral")
-def saudeCerebral():
-        return render_template("saudeMental_Cerebral.html")
-
-@app.route("/saudeMental/saudeAlzheimer")
-def saudeMental_Alzheimer():
-        return render_template("saudeMental_Alzheimer.html")
-
-# botoes pagina minhas memorias
+##################################  MURAL DE HISTÓRIAS  ##############################
+@app.route("/muralDeHistorias")
+def muralDeHistorias():
+        return render_template("muralDeHistorias.html")
+##################################  MINHAS MEMÓRIASS  ##############################
 @app.route("/minhasMemorias")
 def minhasMemorias():
         return render_template("minhasMemorias.html")
@@ -38,14 +33,34 @@ def minhasMemorias():
 def minhasMemorias_adicionarMemoria():
         return render_template("minhasMemorias_adicionarMemoria.html")
 
-@app.route("/minhaGaleria")
+@app.route("/minhasMemorias//minhaGaleria")
 def minhasMemorias_minhaGaleria():
         return render_template("minhasMemorias_minhaGaleria.html")
+
+##################################  AUTOCONHECIMENTO  ##############################
+@app.route("/autoconhecimento")
+def autoconhecimento():
+        return render_template("autoconhecimento.html")
+
+##################################  SAÚDE MENTAL  ##############################
+@app.route("/saudeMental")
+def saudeMental():
+        return render_template("saudeMental.html")
+
+@app.route("/saudeMental/saudeCerebral")
+def saudeCerebral():
+        return render_template("saudeMental_Cerebral.html")
+
+@app.route("/saudeMental/saudeAlzheimer")
+def saudeMental_Alzheimer():
+        return render_template("saudeMental_Alzheimer.html")
+
+
 
 #Teste de template Universal no botão de Jogos cognitivos
 @app.route("/jogosCognitivos")
 def jogosCognitivos():
-        return render_template("saudeMental_templateUniversal.html")
+        return render_template("saudeMental_templateUniversal.html", pagina = "solidao")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
