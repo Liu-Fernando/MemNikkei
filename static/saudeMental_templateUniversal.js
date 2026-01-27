46857d3b3200133650151f64eb92a531b2d19d13
LaurinhaDev <laurapaola2324@gmail.com>
2026-01-27 20:25:27 -0300

    mudança da rota de alguns botões, inclusão da página jogos cognitivos, inclusão das páginas do template (só falta colocar o alt de 2 imagens), mudança do CSS da página dos templates

--- static/saudeMental_templateUniversal.js
+++ static/saudeMental_templateUniversal.js
@@ -8,6 +8,94 @@ const paginasSaudeMental = {
                 textoDescritivoFoto: 'Duas mulheres posam sorrindo diante de um painel colorido de evento. À esquerda, a professora Dra. Deusivania Falcão veste um casaco escuro e mantém uma das mãos na cintura; à direita, Rosângela Marcondes usa óculos, camisa clara, calça estampada e jaqueta verde, também com a mão na cintura. Ambas estão lado a lado, demonstrando proximidade e alegria. Na parte inferior da imagem, lê-se a frase: “Solidão adoece, amizade protege”.',
                 resumo: '“Por que amizade não é luxo: é proteção contra solidão, depressão e até contra adoecimento?”',
                 creditos: 'Dra. Deusivania Falcão e Rosângela Marcondes'
+            }, //tem que separar por vírgula
+            {
+                cor: '#7c83a3',
+                titulo: 'Envelhecimento feminino e solidão',
+                imagem: "../static/Thumbnail_prof_envelhecimento_feminino_e_solidao.png",
+                textoDescritivoFoto: 'Mulher de meia-idade em uma biblioteca, em pé diante de uma estante cheia de livros, lendo um livro aberto. Ela usa blusa branca e blazer escuro. Sobre a imagem há um texto: “Envelhecimento feminino e solidão”.',
+                resumo: '“Compreender os impactos da solidão é fundamental para promover intervenções que fortaleçam o suporte social e emocional ao longo da vida.”',
+                creditos: 'Dra. Deusivania Falcão  '
+            }
+        ]
+    },
+    habitosSaudaveis: {
+        caixinhas: [ //as caixinhas são um array
+            { //os elementos dentro delas são objetos
+                cor: '#8dc31f',
+                titulo: 'Hábitos saudáveis e doença de alzheimer',
+                imagem: "../static/Thumbnail_prof_estimulacao_cognitiva_e_saude_cerebral.jpg",
+                textoDescritivoFoto: 'Mulher adulta sorrindo, usando óculos e camiseta roxa, em frente a uma estante de livros desfocada ao fundo. Sobre a imagem há um destaque em verde com o texto: “Hábitos de vida e doença de Alzheimer” e, abaixo, “Vamos nos cuidar”.',
+                resumo: '“Um estilo de vida saudável pode reduzir o risco e retardar o Alzheimer, mesmo com predisposição genética.”',
+                creditos: 'Dra. Deusivania Falcão'
+            },
+            {
+                cor: '#8dc31f',
+                titulo: 'Envelhecimento e Yoga',
+                imagem: "../static/Thumbnail_prof_envelhecimento_e_yoga.png",
+                textoDescritivoFoto: 'Duas mulheres adultas sorrindo e posando lado a lado em um espaço interno bem iluminado. A mulher da esquerda usa blazer bege e blusa preta; a da direita usa vestido marrom sem mangas. Ao fundo há um banner colorido. Sobre a imagem está escrito: “Envelhecimento e Yoga – Deusivania Falcão & Gaby Scalco”.',
+                resumo: '“A yoga contribui para um envelhecimento ativo e saudável. A prática promove funcionalidade física, equilíbrio emocional e vitalidade cognitiva. Além disso, fortalece o bem-estar e o sentido de vida.”',
+                creditos: 'Dra. Deusivania Falcão e Gaby Scalco '
+            }
+        ]
+    },
+    DoencaDeAlzheimer: {
+        caixinhas: [
+            {
+                cor: '#1e497c',
+                titulo: 'Emoção, comportamento e doença de Alzheimer',
+                imagem: "../static/Thumbnail_prof_emocao_comportamento_e_doenca_de_alzheimer.jpeg",
+                textoDescritivoFoto: 'Mulher adulta sorrindo, usando óculos vermelhos, boné vermelho e moletom rosa, em um ambiente externo com árvores ao fundo. Sobre a imagem há um destaque em amarelo com o texto: "Emoção, comportamento e doença de Alzheimer".', 
+                resumo: '“Desvendando como o Alzheimer transforma emoções e comportamento e aprendendo a cuidar melhor!”',
+                creditos: 'Dra. Deusivania Falcão'
+            },
+            {
+                cor: '#1e497c',
+                titulo: 'Como a arteterapia pode ajudar pessoas com Alzheimer',
+                imagem: "../static/Thumbnail_prof_como_a_arteterapia_pode_ajudar_alzheimer.png",
+                textoDescritivoFoto: 'Mulher adulta sorrindo, de cabelos castanhos e óculos vermelhos, em frente a uma estante cheia de livros. A imagem tem fundo roxo e texto escrito: "Como a arteterapia pode ajudar pessoas com a doença de Alzheimer?"', 
+                resumo: '“A arteterapia é uma intervenção não farmacológica que auxilia o tratamento da doença de Alzheimer.”',
+                creditos: 'Dra. Deusivania Falcão'
+            }
+        ]
+    },
+    saudeCerebral: {
+        caixinhas: [
+            {
+                cor: '#ff9857',
+                titulo: 'Propósito de vida, envelhecimento e velhice',
+                imagem: "../static/Thumbnail_prof_proposito_de_vida_envelhecimento_e_velhice.jpeg",
+                textoDescritivoFoto: 'Imagem com fundo amarelo mostrando uma mulher adulta sentada em uma poltrona, gesticulando enquanto fala em um estúdio de TV. Ela usa roupas escuras e um colar. No canto inferior direito há uma intérprete de Libras. Acima da imagem está escrito: “Propósito de Vida, Envelhecimento e Velhice”. Na parte inferior aparece o nome de usuário “@psi.deusivaniafalcao”.', 
+                resumo: '“Vamos refletir sobre propósitos de vida, envelhecimento e velhice?”',
+                creditos: 'Dra. Deusivania Falcão'
+            },
+            {
+                cor: '#ff9857',
+                titulo: '5 lições sobre longevidade que aprendemos com Abílio Diniz',
+                imagem: "../static/Thumbnail_Abilio_Diniz.png",
+                textoDescritivoFoto: 'Homem idoso de cabelos grisalhos e óculos pretos, visto em close, olhando levemente para o lado. Sobre a imagem há um círculo com o número 5 e o texto: “Lições sobre longevidade que aprendemos com Abílio Diniz”. O fundo é em tom esverdeado.', 
+                resumo: '“A estimulação cognitiva é essencial para manter nosso cérebro saudável”',
+                creditos: 'Abílio Diniz e Dra. Deusivania Falcão'
+            }
+        ]
+    },
+    autoconhecimento: {
+        caixinhas: [
+            {
+                cor: '#f24e75',
+                titulo: 'Hábitos de vida e doença de Alzheimer',
+                imagem: "../static/Thumbnail_prof_habitos_de_vida_e_doenca_alzheimer.png",
+                textoDescritivoFoto: '', 
+                resumo: '“Um estilo de vida saudável pode reduzir o risco e retardar o Alzheimer, mesmo com predisposição genética.”',
+                creditos: 'Dra. Deusivania Falcão'
+            },
+            {
+                cor: '#f24e75',
+                titulo: 'Estimulação cognitiva e saúde cerebral',
+                imagem: "../static/Thumbnail_prof_estimulacao_cognitiva_e_saude_cerebral.jpg",
+                textoDescritivoFoto: '', 
+                resumo: '“A estimulação cognitiva é essencial para manter nosso cérebro saudável”',
+                creditos: 'Dra. Deusivania Falcão'
             }
         ]
     }
@@ -23,6 +111,12 @@ if (config && config.caixinhas) { //verifica se a página existe e se tem as cai
         divCaixa.className = 'caixa'; //adiciona uma classe a essa div criada
         divCaixa.style.backgroundColor = caixinha.cor; //bota uma cor na caixa (a cor definida no array)
 
+        const divTitulo = document.createElement('div'); //cria um elemento texto para colocar o título do vídeo
+        divTitulo.className = 'titulo'; //cria uma classe titulo
+        const p = document.createElement('p'); //cria um elemento paragrafo (p)
+        p.textContent = caixinha.titulo; //o conteúdo textual em p recebe o titulo que tá em caixinha (array)
+        divTitulo.appendChild(p); //coloca o paragrafo dentro da div titulo
+
         const divThumbnail = document.createElement('div'); //cria um elemento div
         divThumbnail.className = 'thumbnail'; //cria uma classe thumbnail 
         const img = document.createElement('img'); //cria um elemento imagem
@@ -30,14 +124,8 @@ if (config && config.caixinhas) { //verifica se a página existe e se tem as cai
         img.alt = caixinha.textoDescritivoFoto; //coloca um texto que descreve a imagem da thumbnail (o texto vai estar no array criado lá em cima)
         divThumbnail.appendChild(img); //coloca a imagem dentro da div thumbnail
 
-        const divTitulo = document.createElement('div'); //cria um elemento texto para colocar o título do vídeo
-        divTitulo.className = 'titulo'; //cria uma classe titulo
-        const p = document.createElement('p'); //cria um elemento paragrafo (p)
-        p.textContent = caixinha.titulo; //o conteúdo textual em p recebe o titulo que tá em caixinha (array)
-        divTitulo.appendChild(p); //coloca o paragrafo dentro da div titulo
-
+        divCaixa.appendChild(divTitulo); //coloca titulo dentro da caixa (tem que ser o 1°)
         divCaixa.appendChild(divThumbnail); //coloca thumbnail dentro da caixa
-        divCaixa.appendChild(divTitulo); //coloca titulo dentro da caixa
 
         container.appendChild(divCaixa);  //bpta a caixa no container (a caixa vai aparecer quando executar isso aqui)
 
