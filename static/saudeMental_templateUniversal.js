fe0b4652a6e1439ec29b5f99acb3daf0234f1b52
LaurinhaDev <laurapaola2324@gmail.com>
2026-01-29 15:40:53 -0300

    mudanças na página ikigai (criação dos outros textos em itálico, botão e da imagem ikigai), inserção dos links nas páginas de saúde mental

--- static/saudeMental_templateUniversal.js
+++ static/saudeMental_templateUniversal.js
@@ -7,7 +7,8 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_prof_solidao_adoece_amizade_protege.png",
                 textoDescritivoFoto: 'Duas mulheres posam sorrindo diante de um painel colorido de evento. À esquerda, a professora Dra. Deusivania Falcão veste um casaco escuro e mantém uma das mãos na cintura; à direita, Rosângela Marcondes usa óculos, camisa clara, calça estampada e jaqueta verde, também com a mão na cintura. Ambas estão lado a lado, demonstrando proximidade e alegria. Na parte inferior da imagem, lê-se a frase: “Solidão adoece, amizade protege”.',
                 resumo: '“Por que amizade não é luxo: é proteção contra solidão, depressão e até contra adoecimento?”',
-                creditos: 'Dra. Deusivania Falcão e Rosângela Marcondes'
+                creditos: 'Dra. Deusivania Falcão e Rosângela Marcondes',
+                link: "https://www.instagram.com/p/DRr9WHLEV68/"
             }, //tem que separar por vírgula
             {
                 cor: '#7c83a3',
@@ -15,7 +16,8 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_prof_envelhecimento_feminino_e_solidao.png",
                 textoDescritivoFoto: 'Mulher de meia-idade em uma biblioteca, em pé diante de uma estante cheia de livros, lendo um livro aberto. Ela usa blusa branca e blazer escuro. Sobre a imagem há um texto: “Envelhecimento feminino e solidão”.',
                 resumo: '“Compreender os impactos da solidão é fundamental para promover intervenções que fortaleçam o suporte social e emocional ao longo da vida.”',
-                creditos: 'Dra. Deusivania Falcão  '
+                creditos: 'Dra. Deusivania Falcão',
+                link: "https://www.instagram.com/p/C512_9CtE-N/"
             }
         ]
     },
@@ -27,7 +29,8 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_prof_estimulacao_cognitiva_e_saude_cerebral.jpg",
                 textoDescritivoFoto: 'Mulher adulta sorrindo, usando óculos e camiseta roxa, em frente a uma estante de livros desfocada ao fundo. Sobre a imagem há um destaque em verde com o texto: “Hábitos de vida e doença de Alzheimer” e, abaixo, “Vamos nos cuidar”.',
                 resumo: '“Um estilo de vida saudável pode reduzir o risco e retardar o Alzheimer, mesmo com predisposição genética.”',
-                creditos: 'Dra. Deusivania Falcão'
+                creditos: 'Dra. Deusivania Falcão',
+                link: "https://www.instagram.com/p/CxTyaOQJLR1/"
             },
             {
                 cor: '#8dc31f',
@@ -35,7 +38,8 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_prof_envelhecimento_e_yoga.png",
                 textoDescritivoFoto: 'Duas mulheres adultas sorrindo e posando lado a lado em um espaço interno bem iluminado. A mulher da esquerda usa blazer bege e blusa preta; a da direita usa vestido marrom sem mangas. Ao fundo há um banner colorido. Sobre a imagem está escrito: “Envelhecimento e Yoga – Deusivania Falcão & Gaby Scalco”.',
                 resumo: '“A yoga contribui para um envelhecimento ativo e saudável. A prática promove funcionalidade física, equilíbrio emocional e vitalidade cognitiva. Além disso, fortalece o bem-estar e o sentido de vida.”',
-                creditos: 'Dra. Deusivania Falcão e Gaby Scalco '
+                creditos: 'Dra. Deusivania Falcão e Gaby Scalco',
+                link: "https://www.instagram.com/p/DQvD9hRD7bC/"
             }
         ]
     },
@@ -47,7 +51,8 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_prof_emocao_comportamento_e_doenca_de_alzheimer.jpeg",
                 textoDescritivoFoto: 'Mulher adulta sorrindo, usando óculos vermelhos, boné vermelho e moletom rosa, em um ambiente externo com árvores ao fundo. Sobre a imagem há um destaque em amarelo com o texto: "Emoção, comportamento e doença de Alzheimer".', 
                 resumo: '“Desvendando como o Alzheimer transforma emoções e comportamento e aprendendo a cuidar melhor!”',
-                creditos: 'Dra. Deusivania Falcão'
+                creditos: 'Dra. Deusivania Falcão',
+                link: "https://www.instagram.com/reel/CvFWJWHAY0b/"
             },
             {
                 cor: '#1e497c',
@@ -55,11 +60,34 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_prof_como_a_arteterapia_pode_ajudar_alzheimer.png",
                 textoDescritivoFoto: 'Mulher adulta sorrindo, de cabelos castanhos e óculos vermelhos, em frente a uma estante cheia de livros. A imagem tem fundo roxo e texto escrito: "Como a arteterapia pode ajudar pessoas com a doença de Alzheimer?"', 
                 resumo: '“A arteterapia é uma intervenção não farmacológica que auxilia o tratamento da doença de Alzheimer.”',
-                creditos: 'Dra. Deusivania Falcão'
+                creditos: 'Dra. Deusivania Falcão',
+                link: "https://www.instagram.com/reel/Cq6dIJipjFt/"
             }
         ]
     },
     saudeCerebral: {
+        caixinhas: [
+            {
+                cor: '#f24e75',
+                titulo: 'Hábitos de vida e doença de Alzheimer',
+                imagem: "../static/Thumbnail_prof_habitos_de_vida_e_doenca_alzheimer.png",
+                textoDescritivoFoto: 'Uma mulher adulta está em pé, olhando para a câmera com expressão acolhedora. Ela tem cabelos longos, castanhos, usa óculos e veste uma camiseta roxa com uma estampa de coração e um cérebro estilizado. O fundo é neutro e desfocado. Sobre a imagem há uma faixa verde com o texto: “Hábitos de vida e doença de Alzheimer”. Abaixo, em outra faixa menor, aparece a frase: “Vamos nos cuidar”.', 
+                resumo: '“Um estilo de vida saudável pode reduzir o risco e retardar o Alzheimer, mesmo com predisposição genética.”',
+                creditos: 'Dra. Deusivania Falcão',
+                link: 'https://www.instagram.com/p/CxTyaOQJLR1/'
+            },
+            {
+                cor: '#f24e75',
+                titulo: 'Estimulação cognitiva e saúde cerebral',
+                imagem: "../static/Thumbnail_prof_estimulacao_cognitiva_e_saude_cerebral.jpg",
+                textoDescritivoFoto: 'Uma mulher adulta aparece em primeiro plano, sorrindo suavemente para a câmera. Ela tem cabelos médios, castanhos claros, soltos, e usa uma blusa laranja com um blazer claro. Ao fundo, vê-se uma estante de livros levemente desfocada, sugerindo um ambiente acadêmico ou biblioteca. Sobre a imagem há uma faixa verde com o texto: “Estimulação Cognitiva”, e logo abaixo, em outra faixa, lê-se: “e saúde cerebral”.', 
+                resumo: '“A estimulação cognitiva é essencial para manter nosso cérebro saudável”',
+                creditos: 'Dra. Deusivania Falcão',
+                link: 'https://www.instagram.com/p/CtIC_s3paaQ/'
+            }
+        ]
+    },
+    autoconhecimento: {
         caixinhas: [
             {
                 cor: '#ff9857',
@@ -67,7 +95,8 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_prof_proposito_de_vida_envelhecimento_e_velhice.jpeg",
                 textoDescritivoFoto: 'Imagem com fundo amarelo mostrando uma mulher adulta sentada em uma poltrona, gesticulando enquanto fala em um estúdio de TV. Ela usa roupas escuras e um colar. No canto inferior direito há uma intérprete de Libras. Acima da imagem está escrito: “Propósito de Vida, Envelhecimento e Velhice”. Na parte inferior aparece o nome de usuário “@psi.deusivaniafalcao”.', 
                 resumo: '“Vamos refletir sobre propósitos de vida, envelhecimento e velhice?”',
-                creditos: 'Dra. Deusivania Falcão'
+                creditos: 'Dra. Deusivania Falcão',
+                link: 'https://www.instagram.com/p/Cx7nqP1uh9G/'
             },
             {
                 cor: '#ff9857',
@@ -75,27 +104,8 @@ const paginasSaudeMental = {
                 imagem: "../static/Thumbnail_Abilio_Diniz.png",
                 textoDescritivoFoto: 'Homem idoso de cabelos grisalhos e óculos pretos, visto em close, olhando levemente para o lado. Sobre a imagem há um círculo com o número 5 e o texto: “Lições sobre longevidade que aprendemos com Abílio Diniz”. O fundo é em tom esverdeado.', 
                 resumo: '“A estimulação cognitiva é essencial para manter nosso cérebro saudável”',
-                creditos: 'Abílio Diniz e Dra. Deusivania Falcão'
-            }
-        ]
-    },
-    autoconhecimento: {
-        caixinhas: [
-            {
-                cor: '#f24e75',
-                titulo: 'Hábitos de vida e doença de Alzheimer',
-                imagem: "../static/Thumbnail_prof_habitos_de_vida_e_doenca_alzheimer.png",
-                textoDescritivoFoto: '', 
-                resumo: '“Um estilo de vida saudável pode reduzir o risco e retardar o Alzheimer, mesmo com predisposição genética.”',
-                creditos: 'Dra. Deusivania Falcão'
-            },
-            {
-                cor: '#f24e75',
-                titulo: 'Estimulação cognitiva e saúde cerebral',
-                imagem: "../static/Thumbnail_prof_estimulacao_cognitiva_e_saude_cerebral.jpg",
-                textoDescritivoFoto: '', 
-                resumo: '“A estimulação cognitiva é essencial para manter nosso cérebro saudável”',
-                creditos: 'Dra. Deusivania Falcão'
+                creditos: 'Abílio Diniz e Dra. Deusivania Falcão',
+                link: 'https://www.instagram.com/p/C3jFWYuNo7d/'
             }
         ]
     }
@@ -110,6 +120,10 @@ if (config && config.caixinhas) { //verifica se a página existe e se tem as cai
         const divCaixa = document.createElement('div'); //cria um elemento div
         divCaixa.className = 'caixa'; //adiciona uma classe a essa div criada
         divCaixa.style.backgroundColor = caixinha.cor; //bota uma cor na caixa (a cor definida no array)
+        divCaixa.style.cursor = 'pointer'; //para o cursor mudar quando aproximar da div 
+        divCaixa.onclick = function() { //função para a gente clicar
+            window.location.href = caixinha.link; //pega o link no array
+        }
 
         const divTitulo = document.createElement('div'); //cria um elemento texto para colocar o título do vídeo
         divTitulo.className = 'titulo'; //cria uma classe titulo
@@ -124,9 +138,11 @@ if (config && config.caixinhas) { //verifica se a página existe e se tem as cai
         img.alt = caixinha.textoDescritivoFoto; //coloca um texto que descreve a imagem da thumbnail (o texto vai estar no array criado lá em cima)
         divThumbnail.appendChild(img); //coloca a imagem dentro da div thumbnail
 
+        
         divCaixa.appendChild(divTitulo); //coloca titulo dentro da caixa (tem que ser o 1°)
         divCaixa.appendChild(divThumbnail); //coloca thumbnail dentro da caixa
 
+        //container.appendChild(linkVideo); //coloca o link dentro do container 
         container.appendChild(divCaixa);  //bpta a caixa no container (a caixa vai aparecer quando executar isso aqui)
 
         const divResumo = document.createElement('div'); //cria uma div para o resumo
