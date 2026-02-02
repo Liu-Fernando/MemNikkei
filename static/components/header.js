3c020441c27936122719ab00ead4d8674b9ae463
erikye02 <yzs0111@gmail.com>
2026-02-02 16:26:58 -0300

    menu

--- static/components/header.js
+++ static/components/header.js
@@ -6,15 +6,16 @@ headerTemplate.innerHTML = `
         font-family: 'Noto Serif Japanese';
         background-color: #d52229;
         color: #ffffff;
-        font-size: 10vw;
+        font-size: 8vw;
         width: 100%;
         
         
-        
-       
+        justify-content: space-between;
+        position: relative;
         display:flex;
         //TO-DO Alinhar
     }
+
     a{
     all: unset;
     font-family:Noto Serif Japanese' ;
@@ -23,17 +24,118 @@ headerTemplate.innerHTML = `
     align-height:center;
     
     }
+
+    div{
+    display: flex;
+    align-items: center;
+
+    }
+
+    ul{
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-itens:center;
+        gap: 5px;
+    }
+
+    .menu-button{
+        font-family: 'Noto Serif Japanese';
+        background-color: #d52229;
+        color: #ffffff;
+        font-size: 8vw;
+    }
+
+    .close-menu-button{
+    
+        position: absolute;
+        top: 20px;
+        left: 35px;
+        width: 20vw;
+        height: 7vh;
+        cursor: pointer;
+    }
+
     .logoHeader{
     
     width:18vw;
-    
+    }
+
+    a.btn{
+    all: unset;
+    box-sizing: border-box;
+
+    font-family: Inter;
+    width: 80vw;
+    height: 20vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 5px;
+
+    background-color: #1e2a38;
+    color: white;
+
+    padding: 10px 15px;
+    border-radius: 10px;
+    font-size: 8vw;
+    font-weight: bold;
+    text-align: center;
+
+    text-decoration: none;
+    cursor: pointer;
+    }
+
+    .close-menu-button{
+        top: 20px;
+        right: 20px;
+        bottom-margin: 20px;
+        position: absolute;
+        font-size: 20px;
+    }
+
+    .nav-menu{
+    display: none;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: 300vh;
+    background-color: rgb(226, 27, 27);
+    justify-content: center;
+    padding-top: 80px;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+    max-height: 100vh;
+    }
+
+    .nav-menu.active{
+    display:flex;
     }
 
 </style>
     <header>
-    <img src="../static/MemLogo.png " class="logoHeader" alt="Logo"> 
-        <a href="/paginaInicial">Memórias Nikkei</a>
-        
+        <div>
+            <img src="../static/MemLogo.png " class="logoHeader" alt="Logo"> 
+            <a href="/paginaInicial">Memórias Nikkei</a>
+        </div>
+        <button class="menu-button">☰</button>
+
+        <nav class="nav-menu">
+            <button class="close-menu-button">←</button>
+            <ul>
+                <li><a href="" class="btn">Voltar ao menu inicial</a></li>
+                <li><a href="" class="btn">Relembrar tradições</a></li>
+                <li><a href="" class="btn">Mural de histórias</a></li>
+                <li><a href="" class="btn">Minhas memórias</a></li>
+                <li><a href="" class="btn">Autoconhecimento</a></li>
+                <li><a href="" class="btn">Saúde mental e Longevidade</a></li>
+                <li><a href="" class="btn">Jogos cognitivos</a></li>
+                <li><a href="" class="btn">Conheça o projeto</a></li>
+            </ul>
+        </nav>
     </header>
 
 
@@ -50,6 +152,20 @@ class Header extends HTMLElement{
         const shadowRoot = this.attachShadow({mode:'closed'});
 
         shadowRoot.appendChild(headerTemplate.content);
+        const btun = shadowRoot.querySelector('.menu-button')
+        const navMenu = shadowRoot.querySelector('.nav-menu')
+        const closeBtun = shadowRoot.querySelector('.close-menu-button')
+
+        btun.addEventListener('click', ()=> {
+            navMenu.classList.toggle('active');
+            document.body.style.overflow = 'hidden'
+        });
+
+        closeBtun.addEventListener('click', ()=> {
+            navMenu.classList.remove('active');
+            document.body.style.overflow = ''
+        }); 
+
         
     }
 }
