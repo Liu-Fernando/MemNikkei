391c7f0223490d6d0c2f3408b30789ea78dd566c
LaurinhaDev <laurapaola2324@gmail.com>
2026-03-29 18:34:37 -0300

    muitas novas fotos, esportes e conquistas

---
+++ static/esportes.js
@@ -0,0 +1,236 @@
+const paginaEsportes = {
+    beisebol: {
+        esporte: [
+            {
+                nome: "Beisebol (Yakyū)",
+                texto1: "Símbolo comunitário introduzido pelos imigrantes japoneses, o beisebol consolidou-se como uma das principais expressões esportivas da comunidade nikkei no Brasil.",
+
+                foto1: "/static/luvaBeisebol.jpg",
+                descricaoFoto1: "uau",
+
+                textoLinhaDoTempo: "Linha do Tempo",
+                arrayLinhaDoTempo: [
+                    {tipo: "ano", conteudo: "1908"},
+                    {tipo: "texto", conteudo: "Primeiros equipamentos chegam com o navio Kasato Maru, trazidos pelo imigrante Samejima"},
+
+                    {tipo: "ano", conteudo: "1910-1920"},
+                    {tipo: "texto", conteudo: "Kenji Sassawara funda o Mikado, primeiro time organizado da colônia japonesa no Brasil."},
+
+                    {tipo: "ano", conteudo: "1920"},
+                    {tipo: "texto", conteudo: "Isamu Yuba leva o beisebol ao interior paulista, expandindo a pr para comunidades agrícolas."},
+
+                    {tipo: "ano", conteudo: "1936"},
+                    {tipo: "texto", conteudo: "Realiza-se o Primeiro Torneio Nacional."},
+
+                    {tipo: "ano", conteudo: "1941-1945"},
+                    {tipo: "texto", conteudo: "A Segunda Guerra Mundial interrompe as atividades esportivas."},
+
+                    {tipo: "ano", conteudo: "1946"},
+                    {tipo: "texto", conteudo: "Fundação da Federação Paulista de Beisebol e Softbol, marcando a retomada institucional do esporte."},
+                ],
+
+                textoFuncaoSocial: "Função social do Beisebol",
+                texto2: "Para a comunidade nikkei no Brasil, o beisebol era um espaço de confraternização, integrando  regiões agrícolas a centros urbanos e fortalecendo os laços entre eles.",
+
+                textoClubesReferencia: "Clubes de referência",
+                arrayClubes: [
+                    "Nikkei Club Marília (Marília – SP) – Octacampeão brasileiro",
+                    "Nippon Blue Jays"
+                ],
+
+                textoMarcoPatrimonial: "Marco patrimonial",
+                arrayMarcos: [
+                    "Estádio Mie Nishi (Bom Retiro, São Paulo)"
+                ],
+
+                foto2: "/static/quadraBeisebol.jpg",
+                descricaoFoto2: "uau"
+            }
+        ]
+    },
+    judo: {
+        esporte: [
+            {
+                nome: "Judô (Caminho Suave)",
+                texto1: "  Fundado em 1882 por Jigoro Kano, o judô, cujo nome pode ser traduzido como “caminho suave”, nasceu como uma prática que une técnica e formação moral. O esporte propõe um percurso de autodesenvolvimento guiado pela disciplina, pelo respeito e pela convivência harmoniosa.",
+
+                foto1: "/static/judo1.jpg",
+                descricaoFoto1: "uau",
+
+                textoLinhaDoTempo: "Linha do Tempo",
+                arrayLinhaDoTempo: [
+                    {tipo: "ano", conteudo: "1922"},
+                    {tipo: "texto", conteudo: "Eisei Maeda apresenta o esporte no Brasil."},
+
+                    {tipo: "ano", conteudo: "1938"},
+                    {tipo: "texto", conteudo: "Riuzo Ogawa funda a Academia Ogawa e fortalece a prática."},
+
+                    {tipo: "ano", conteudo: "1969"},
+                    {tipo: "texto", conteudo: "Criação da Confederação Brasileira de Judô."},
+
+                    {tipo: "ano", conteudo: "1972"},
+                    {tipo: "texto", conteudo: "Chiaki Ishii conquista a primeira medalha olímpica brasileira no judô."},
+
+                    {tipo: "ano", conteudo: "2008"},
+                    {tipo: "texto", conteudo: "Ketelyn Quadros obtém a primeira medalha olímpica feminina do país."},
+                ],
+
+                textoFuncaoSocial: "Função social do Judô",
+                texto2: "No Brasil, o judô tornou-se instrumento de formação e inclusão social. Ao unir a arte marcial e os valores éticos, o judô contribui para o desenvolvimento pessoal de crianças, jovens e adultos, fortalecendo vínculos comunitários e perpetuando a herança cultural japonesa no país.",
+
+                textoClubesReferencia: "Clubes de referência",
+                arrayClubes: [
+                    "Esporte Clube Pinheiros (SP) – um dos maiores formadores de atletas olímpicos",
+                    "Clube Paineiras do Morumby (SP)",
+                    "Minas Tênis Clube (Belo Horizonte – MG)",
+                    "Instituto Reação (RJ)"
+                ],
+
+                textoMarcoPatrimonial: "Marco patrimonial",
+                arrayMarcos: [
+                    "Centro Pan-Americano de Judô, em Lauro de Freitas (BA): principal centro de treinamento da modalidade no país.",
+                    "Academias históricas fundadas por mestres japoneses, que preservaram a tradição técnica."
+                ],
+
+                foto2: "/static/judo2.jpeg",
+                descricaoFoto2: "uau"
+            }
+        ]
+    },
+    gateball: {
+        esporte: [
+            {
+                nome: "Gateball (Portão de Bola)",
+                texto1: "Criado no Japão em 1947 por Eiji Suzuki, na província de Hokkaido, o gateball surgiu no pós-guerra como atividade recreativa acessível às crianças. \n Inspirado em jogos britânicos, mas adaptado à realidade japonesa(pouco espaço e alta densidade populacional) rapidamente passou a atrair também pessoas mais velhas.",
+
+                foto1: "/static/gateball1.jpeg",
+                descricaoFoto1: "uau",
+
+                textoLinhaDoTempo: "Linha do Tempo",
+                arrayLinhaDoTempo: [
+                    {tipo: "ano", conteudo: "1978"},
+                    {tipo: "texto", conteudo: "O contato com o esporte. \n Durante viagem ao Japão, Matsumi Kuroki conhece a modalidade e decide introduzi-la no Brasil."},
+
+                    {tipo: "ano", conteudo: "1979"},
+                    {tipo: "texto", conteudo: "Primeiro jogo em equipe no campo da Associação de Jovens de Fukuhaku, em Suzano (SP)."},
+
+                    {tipo: "ano", conteudo: "1981"},
+                    {tipo: "texto", conteudo: "Primeiro Torneio de Gateball do Brasil."},
+
+                    {tipo: "ano", conteudo: "1982"},
+                    {tipo: "texto", conteudo: "Criação do primeiro órgão coordenador do gateball, em reunião na Associação CR Nagano Kanjin do Brasil (Liberdade – SP)."},
+
+                    {tipo: "ano", conteudo: "1990"},
+                    {tipo: "texto", conteudo: "Construção do primeiro campo oficial em São Paulo, no Clube da Cidade Vila Guarani, hoje com 20 campos dedicados à modalidade."},
+                ],
+
+                textoFuncaoSocial: "Função social do Gateball",
+                texto2: "O gateball une a convivência comunitária ao longo do envelhecimento na comunidade nikkei. \n   No Brasil, a modalidade estimula a atividade física adaptada à terceira idade e integra os vínculos culturais da imigração japonesa, criando espaços de encontre entre diferentes gerações. \n Cultivando a memória e o pertencimento, o gateball preserva tradições e valores dos antepassados. A prática garante que a herança cultural nikkei permaneça viva, renovando-se ao acolher novos participantes nas comunidades brasileiras.",
+
+                textoClubesReferencia: "Clubes de referência",
+                arrayClubes: [
+                    "Associação de Jovens de Fukuhaku (Suzano – SP)",
+                    "Hakuju-kai de Itapeti",
+                    "Associação CR Nagano Kanjin do Brasil (Liberdade – SP)",
+                    "Clube da Cidade Vila Guarani (SP)"
+                ],
+
+                textoMarcoPatrimonial: "Marco patrimonial",
+                arrayMarcos: [
+                    "O Clube da Cidade Vila Guarani, na zona sul de São Paulo, tornou-se o principal marco urbano do gateball brasileiro, com dezenas de campos dedicados exclusivamente à prática."
+                ],
+
+                foto2: "/static/gateball2.jpg",
+                descricaoFoto2: "uau"
+            }
+        ]
+    }
+}
+
+const container = document.getElementById('container'); //pega a div do html que tem container
+const configuracao = paginaEsportes[paginaAtual];//pega a config da página atual. Se a página atual 
+
+if (configuracao && configuracao.esporte) { 
+    configuracao.esporte.forEach(esporte => { 
+
+        const nome = document.createElement('p');
+        nome.className = 'nomeEsporte';
+        nome.textContent = esporte.nome;
+        container.appendChild(nome);
+
+        const texto1 = document.createElement('p');
+        texto1.className = 'texto';
+        texto1.textContent = esporte.texto1;
+        container.appendChild(texto1);
+
+        const img1 = document.createElement('img');
+        img1.className = 'imagem';
+        img1.src = esporte.foto1;
+        img1.alt = esporte.descricaoFoto1;
+        container.appendChild(img1);
+
+        const textoLinhaDoTempo = document.createElement('p');
+        textoLinhaDoTempo.className = 'textoNegrito';
+        textoLinhaDoTempo.textContent = esporte.textoLinhaDoTempo;
+        container.appendChild(textoLinhaDoTempo);
+
+        esporte.arrayLinhaDoTempo.forEach(item => {
+            if (item.tipo === "ano") {
+                const p = document.createElement('p');
+                p.className = 'ano';
+                p.textContent = item.conteudo;
+                container.appendChild(p);
+
+            } else if (item.tipo === "texto") {
+                const p = document.createElement('p');
+                p.className = 'textoLinhaDoTempo';
+                p.textContent = item.conteudo;
+                container.appendChild(p);
+            }
+        });
+
+        const textoFuncaoSocial = document.createElement('p');
+        textoFuncaoSocial.className = 'textoNegrito';
+        textoFuncaoSocial.textContent = esporte.textoFuncaoSocial;
+        container.appendChild(textoFuncaoSocial);
+
+        const texto2 = document.createElement('p');
+        texto2.className = 'texto';
+        texto2.textContent = esporte.texto2;
+        container.appendChild(texto2);
+
+        const textClubesReferencia = document.createElement('p');
+        textClubesReferencia.className = 'textoNegrito';
+        textClubesReferencia.textContent = esporte.textoClubesReferencia;
+        container.appendChild(textClubesReferencia);
+
+        const ul1 = document.createElement('ul');
+        esporte.arrayClubes.forEach(item => {
+            const itemLista = document.createElement('li');
+            itemLista.textContent = item;
+            ul1.appendChild(itemLista);
+        });
+        container.appendChild(ul1);
+
+        const textoMarcoPatrimonial = document.createElement('p');
+        textoMarcoPatrimonial.className = 'textoNegrito';
+        textoMarcoPatrimonial.textContent = esporte.textoMarcoPatrimonial;
+        container.appendChild(textoMarcoPatrimonial);
+
+        const ul2 = document.createElement('ul');
+        esporte.arrayMarcos.forEach(item => {
+            const itemLista = document.createElement('li');
+            itemLista.textContent = item;
+            ul2.appendChild(itemLista);
+        });
+        container.appendChild(ul2);
+
+        const img2 = document.createElement('img');
+        img2.className = 'imagem';
+        img2.src = esporte.foto2;
+        img2.alt = esporte.descricaoFoto2;
+        container.appendChild(img2);
+
+    }
+    )
+}
